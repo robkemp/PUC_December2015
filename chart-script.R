7f53586c746d6ed85cae56ad5178f165177ccbaf
Rob Kemp <rjameskemp@gmail.com>
2015-12-08 21:38:28 -0700

    added households by age chart

--- chart-script.R
+++ chart-script.R
@@ -6,7 +6,7 @@ library(grid)
 library(scales)
 
 
-pph_co=readxl::read_excel("J:/Projections/Households/totalPphd1970to2050.xlsx")
+pph_co=readxl::read_excel("totalPphd1970to2050.xlsx")
 
 
 p=pph_co%>%
@@ -20,10 +20,50 @@ p
 ggsave("personsPerHousehold_co.png", p, h=150, w=250, units="mm")
 
 
-hh=readxl::read_excel("J:/Projections/Households/hhdata_transformV2014.xlsx")
+hh=readxl::read_excel("hhdata_transformV2014.xlsx")
 
 
 hh_county=hh%>%
   rename(countyfips=area_code, year=Year, totalHouseholds=total_households)%>%
   group_by(countyfips, year)%>%
   summarize(totalHouseholds=sum(totalHouseholds))
+
+yrs=c(2010, 2015, 2020, 2025, 2030)
+
+hh_age=hh%>%
+  rename(countyfips=area_code, year=Year, totalHouseholds=total_households)%>%
+  filter(year %in% yrs)%>%
+  group_by( year, age_group_id)%>%
+  summarize(totalHouseholds=sum(totalHouseholds))%>%
+  ungroup()%>%
+  mutate(age_cat=ordered(age_group_id, levels = 1:4,
+                         labels = c("18 to 24", "25 to 44", "45 to 64", "65 and over" )))%>%
+  group_by(year)%>%
+  mutate(share=totalHouseholds/sum(totalHouseholds))
+
+hh_age_test=hh%>%
+  rename(countyfips=area_code, year=Year, totalHouseholds=total_households)%>%
+  filter(year==2030)%>%
+  group_by(age_group_id)%>%
+  summarize(totalHouseholds=sum(totalHouseholds))%>%
+  mutate(share=totalHouseholds/sum(totalHouseholds))
+
+codemog_pal=c(rgb(31,73,125, max=255),
+              rgb(192,80,77, max=255),
+              rgb(101, 80, 60, max=255),
+              rgb(239, 117, 33, max=255),
+              rgb(119, 171, 67, max = 255),
+              rgb(208, 210, 211, max = 255),
+              rgb(210, 210, 210, max = 255))
+
+p2=hh_age%>%
+  ggplot(aes(x=year, y=share, color=as.factor(age_cat)))+
+  geom_line(size=1.25)+
+  geom_point(size=2.25)+
+  scale_y_continuous(labels=percent)+
+  scale_colour_manual(name="",values=codemog_pal)+
+  theme_codemog(base_size = 15)+
+  labs(x="Year", y="Share of Households", title= "Colorado Household Projections by Age\nSource: State Demography Office")
+p2
+
+ggsave("households_age.png", p2, h=150, w=250, units="mm")
